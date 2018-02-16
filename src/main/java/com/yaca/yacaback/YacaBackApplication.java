90bf17931fdd6fe77fdaa076968e34b3387ce96e
olivier.texier <olivier.texier@adventiel.fr>
2018-02-16 16:51:19 +0100

    nux

---
+++ src/main/java/com/yaca/yacaback/YacaBackApplication.java
@@ -0,0 +1,12 @@
+package com.yaca.yacaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YacaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YacaBackApplication.class, args);
+	}
+}
